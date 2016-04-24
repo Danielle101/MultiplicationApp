b6d89690dd382ca5e7268553adf0faf9b06211cd
DanielleHall <halldan4@yahoo.com>
2016-04-23 21:21:18 -0400

    updated validator class and cleaned main

--- src/multiplyBy/multiplicationApp.java
+++ src/multiplyBy/multiplicationApp.java
@@ -9,15 +9,10 @@ System.out.println("Welcome to John's Multiplication App!\n");
 System.out.println("Enter any number between 1 & 50 and receive the first 10 multiples of"
 		+ "that number.");
 System.out.println("Please enter a number (1-50):");
-int userInput = scan.nextInt();
+int userInput = Validator.getValidInt(1, 50);
 System.out.println("");
 //return userInput;
-for (int i = 1; i<11; i++){
-int multiply = userInput*i;	
-System.out.println(userInput + " multiplied by "+ i + " is "+ multiply+"."); 
-}	
-
-
+getMath.getMultiples(userInput);
 }
 }
 
